a169c5417465f9318ac9c11825f22904cdcd4873
AnastasiaDanilenko <451005asya@gmail.com>
2016-09-12 22:41:20 +0300

    Comparator added

---
+++ QuickSort/Comparator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Comparator:IComparer<int>
+    {
+        public int Compare(int first, int second)
+        {
+            if (first > second)
+                return 1;
+            else
+            {
+                if (first == second)
+                    return 0;
+                else
+                    return -1;
+            }
+        }
+
+    }
+}
--- QuickSort/QuickSortClass.cs
+++ QuickSort/QuickSortClass.cs
@@ -8,6 +8,23 @@ namespace QuickSort
 {
     class QuickSortClass
     {
+        private 
+        private int amountParameters;
+        private int[] arrayToSort;
 
+        public QuickSortClass(int amount)
+        {
+            amountParameters = amount;
+        }
+
+        public bool Compare(int first, int second)
+        {
+            return ;
+        }
+
+        public void GenerateArray()
+        {
+
+        }
     }
 }
